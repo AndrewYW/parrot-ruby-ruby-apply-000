a81eb4585d72c12fd45f1e585beb68fe69b8c503
Andrew Wang <andrewwang39@gmail.com>
2018-09-28 20:40:49 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,12 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(str)
-  if str == "" || str == nil
-    puts "Squawk!"
-    return "Squawk!"
-  else
-    puts str
-    str
-  end
+def parrot(str = "Squawk!")
+  puts "#{str}"
+  return str
 end
